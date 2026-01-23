6714dadc3af298b62e4ec062dec76faf3a96f499
OmAmberkar <omamberkar08@gmail.com>
2026-01-23 14:25:34 +0530

    fix: landing page image fixed

--- src/components/ui/DashboardSample.tsx
+++ src/components/ui/DashboardSample.tsx
@@ -1,10 +1,10 @@
 const DashBoardSample = () => {
   return (
-    <div className='shadow-[10px_10px_10px_rgba(59,130,246,0.3)] rounded-2xl hover:border-blue-600 hover:shadow-[20px_20px_30px_rgba(59,130,246,0.5)] transition-all duration-300 border-2 border-transparent overflow-hidden bg-blue-950/20 backdrop-blur-xl p-4 md:p-8 lg:p-12'>
+    <div className='shadow-[10px_10px_10px_rgba(59,130,246,0.3)] rounded-2xl hover:border-blue-600 hover:shadow-[20px_20px_30px_rgba(59,130,246,0.5)] transition-all duration-300 border-2 border-transparent overflow-hidden  bg-gray-100 backdrop-blur-xl  p-4 md:p-8 lg:p-1'>
       <img
         src='/DemoImage.png'
         alt='OpenConsole Demo Image'
-        className='w-fit h-fit mx-auto my-auto'
+        className='h-full w-full rounded-2xl'
       />
     </div>
   );
